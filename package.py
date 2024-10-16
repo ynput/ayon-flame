37241b3975b5cece0722cbb73b3fd47151b4786a
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-16 10:31:35 +0200

    define 'app_host_name'

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "flame"
 title = "Flame"
 version = "0.2.4+dev"
-
+app_host_name = "flame"
 client_dir = "ayon_flame"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
