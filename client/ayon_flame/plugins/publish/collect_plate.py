9e4a05927bf07e2ed613d71b4e409f372acdf9b0
robin@ynput.io <robin@ynput.io>
2024-12-03 17:27:37 -0500

    Report fix clip_media reviewable source.

--- client/ayon_flame/plugins/publish/collect_plate.py
+++ client/ayon_flame/plugins/publish/collect_plate.py
@@ -29,18 +29,52 @@ class CollectPlate(pyblish.api.InstancePlugin):
 
         instance.data["otioClip"] = otio_clip
 
+        # solve reviewable options
         review_switch = instance.data["creator_attributes"].get(
             "review")
         reviewable_source = instance.data["creator_attributes"].get(
             "reviewableSource")
 
-        if review_switch:
+        if review_switch is True:
             if reviewable_source == "clip_media":
                 instance.data["families"].append("review")
+                instance.data.pop("reviewTrack", None)
+
+                # Retrieve source clip media from otio clip.
+                # image sequence
+                if hasattr(otio_clip.media_reference, "target_url_for_image_number"):
+                    file_url = otio_clip.media_reference.target_url_for_image_number(0)
+                    sequence_length = (
+                        otio_clip.media_reference.end_frame()
+                        - otio_clip.media_reference.start_frame
+                    )
+                    file_names = [
+                        os.path.basename(
+                            otio_clip.media_reference.target_url_for_image_number(frame)
+                        )
+                        for frame in range(0, sequence_length)
+                    ]
+
+                # movie
+                else:
+                    file_url = otio_clip.media_reference.target_url
+                    file_names = os.path.basename(file_url)
+
+                _, ext = os.path.splitext(file_url)
+
+                repre = {
+                    "name": ext[1:],
+                    "ext": ext[1:],
+                    "files": file_names,
+                    "stagingDir": os.path.dirname(file_url),
+                    "tags": ["review", "delete"]
+                }
+                instance.data["representations"].append(repre)
+
             else:
                 instance.data["reviewTrack"] = reviewable_source
 
-        # remove review keys from instance data
+        # remove creator-specific review keys from instance data
         instance.data.pop("reviewableSource", None)
         instance.data.pop("review", None)
 
