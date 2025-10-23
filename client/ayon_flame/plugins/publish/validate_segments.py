9040cfa2fb8120db6cf3f9cdcfad9dfaceeaf78d
Jakub Jezek <jakubjezek001@gmail.com>
2025-10-23 16:16:54 +0200

    adding segments validation

---
+++ client/ayon_flame/plugins/publish/validate_segments.py
@@ -0,0 +1,32 @@
+import pyblish.api
+from ayon_core.pipeline.publish import PublishValidationError
+
+
+class ValidateSegments(pyblish.api.ContextPlugin):
+    """Inject the current working context into publish context"""
+
+    label = "Validate Segments"
+    order = pyblish.api.ValidateOrder
+
+    def process(self, context):
+        failed_segments = context.data["failedSegments"]
+
+        if not failed_segments:
+            return
+
+        msg = "Timeline Clips failing validation:"
+        msg_html = "## Following clips are failing validation:"
+        for segment in failed_segments:
+            shot_name = segment.shot_name.get_value()
+            segment_name = segment.name.get_value()
+            clip_msg = (
+                f"Clip name: {segment_name} with shot name: {shot_name}")
+            msg += f"\n{clip_msg}"
+
+            msg_html += f"<br> - {clip_msg}"
+
+        raise PublishValidationError(
+            title="Missing correctsegments attributes",
+            message=msg,
+            description=msg_html
+        )
