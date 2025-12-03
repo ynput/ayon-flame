e7f99eb2717e3c50aaf07a49a81de2494918f91c
jakubjezek001 <jakubjezek001@gmail.com>
2025-12-03 16:57:23 +0100

    Refactor: Convert plate creator to new FlameCreator
    Convert the plate creator to inherit from FlameCreator and follow the
    new creator pattern.

--- client/ayon_flame/plugins/create/create_plate.py
+++ client/ayon_flame/plugins/create/create_plate.py
@@ -16,19 +16,18 @@ Restrictions:
     - selected mode only supported so precreate validation needs to check
       if selected items are valid for plate creation and raise if no selection
 """
-import ayon_flame.api as ayfapi
+import uuid
+from copy import deepcopy
+
 import flame
-from ayon_core.lib import BoolDef, EnumDef, NumberDef, TextDef, UILabelDef
-from ayon_core.pipeline.create import (
-    CreatedInstance,
-    CreatorError,
-    ParentFlags,
-)
+from ayon_core.lib import BoolDef
 from ayon_flame.api import lib, pipeline, plugin
+from ayon_core.pipeline.create import CreatedInstance, CreatorError
 
 # Used as a key by the creators in order to
 # retrieve the instances data into clip markers.
 _CONTENT_ID = "flame_sub_products"
+
 
 # get current selection from the current focus
 # TODO: implement it in plugin
@@ -44,193 +43,105 @@ def get_segments_from_selection():
     ]
 
 
-class _FlameInstanceCreator(plugin.HiddenFlameCreator):
-    """Wrapper class for clip types products.
-    """
+class PlateCreator(plugin.FlameCreator):
+    """Publishable clip"""
+    identifier = "io.ayon.creators.flame.plate"
+    product_type = "plate"
+    label = "Plate Reel Clip"
 
-    def _add_instance_to_context(self, instance):
-        parent_id = instance.get("parent_instance_id")
-        if parent_id is not None and ParentFlags is not None:
-            instance.set_parent(
-                parent_id,
-                # Disable if a parent is disabled and delete if a parent
-                #   is deleted
-                ParentFlags.share_active | ParentFlags.parent_lifetime
-            )
-        super()._add_instance_to_context(instance)
+    icon = "film"
+    defaults = ["Main"]
 
-    def create(self, instance_data, _):
-        """Return a new CreateInstance for new shot from Flame.
+    detailed_description = """
+Publishing clips/plate, audio for new shots to project
+or updating already created from Flame. Publishing will create
+OTIO file.
+"""
 
-        Args:
-            instance_data (dict): global data from original instance
+    def get_pre_create_attr_defs(self):
 
-        Return:
-            CreatedInstance: The created instance object for the new shot.
-        """
-
-        new_instance = CreatedInstance(
-            self.product_type,
-            instance_data["productName"],
-            instance_data,
-            self
-        )
-        self._add_instance_to_context(new_instance)
-        return new_instance
-
-    def update_instances(self, update_list):
-        """Store changes of existing instances so they can be recollected.
-
-        Args:
-            update_list(List[UpdateData]): Gets list of tuples. Each item
-                contain changed instance and it's changes.
-        """
-        for created_inst, _changes in update_list:
-            clip_item = created_inst.transient_data["clip_item"]
-            marker_data = ayfapi.get_clip_data_marker(clip_item)
-
-            # Backwards compatible (Deprecated since 24/09/05)
-            # ignore instance if no existing marker data
-            if marker_data is None:
-                continue
-
-            try:
-                instances_data = marker_data[_CONTENT_ID]
-
-            # Backwards compatible (Deprecated since 24/09/05)
-            except KeyError:
-                marker_data[_CONTENT_ID] = {}
-                instances_data = marker_data[_CONTENT_ID]
-
-            instances_data[self.identifier] = created_inst.data_to_store()
-            pipeline.imprint(
-                clip_item,
-                data=  {
-                    _CONTENT_ID: instances_data,
-                    "clip_index": marker_data["clip_index"],
-                }
-            )
-
-    def remove_instances(self, instances):
-        """Remove instance marker from track item.
-
-        Args:
-            instance(List[CreatedInstance]): Instance objects which should be
-                removed.
-        """
-        for instance in instances:
-            clip_item = instance.transient_data["clip_item"]
-            marker_data = ayfapi.get_clip_data_marker(clip_item)
-
-            instances_data = marker_data.get(_CONTENT_ID, {})
-            instances_data.pop(self.identifier, None)
-            self._remove_instance_from_context(instance)
-
-            pipeline.imprint(
-                clip_item,
-                data=  {
-                    _CONTENT_ID: instances_data,
-                    "clip_index": marker_data["clip_index"],
-                }
-            )
-
-
-class _FlameInstanceClipCreatorBase(_FlameInstanceCreator):
-    """ Base clip product creator.
-    """
-
-    def register_callbacks(self):
-        self.create_context.add_value_changed_callback(self._on_value_change)
-
-    def _on_value_change(self, event):
-        for item in event["changes"]:
-            instance = item["instance"]
-            if (
-                instance is None
-                or instance.creator_identifier != self.identifier
-            ):
-                continue
-
-            changes = item["changes"].get("creator_attributes", {})
-            if "review" not in changes:
-                continue
-
-            attr_defs = instance.creator_attributes.attr_defs
-            review_value = changes["review"]
-            reviewable_source = next(
-                attr_def
-                for attr_def in attr_defs
-                if attr_def.key == "reviewableSource"
-            )
-            reviewable_source.enabled = review_value
-
-            instance.set_create_attr_defs(attr_defs)
-
-    def get_attr_defs_for_instance(self, instance):
-        parent_instance = instance.creator_attributes.get("parent_instance")
-        current_sequence = lib.get_current_sequence(lib.CTX.selection)
-
-        if current_sequence is not None:
-            gui_tracks = [
-                {"value": tr_name, "label": f"Track: {tr_name}"}
-                for tr_name in get_video_track_names(current_sequence)
-            ]
-        else:
-            gui_tracks = []
-
-        instance_attributes = [
-            TextDef(
-                "parentInstance",
-                label="Linked to",
-                disabled=True,
-                default=parent_instance,
+        return [
+            BoolDef(
+                "use_selection",
+                label="Use only selected clip(s).",
+                tooltip=(
+                    "When enabled it restricts create process "
+                    "to selected clips."
+                ),
+                default=True
             ),
         ]
 
-        if self.product_type == "plate":
-            current_review = instance.creator_attributes.get("review", False)
-            instance_attributes.extend(
-                [
-                    BoolDef(
-                        "review",
-                        label="Review",
-                        tooltip="Switch to reviewable instance",
-                        default=False,
-                    ),
-                    EnumDef(
-                        "reviewableSource",
-                        label="Reviewable Source",
-                        tooltip=("Selecting source for reviewable files."),
-                        items=(
-                            [
-                                {
-                                    "value": "clip_media",
-                                    "label": "[ Clip's media ]",
-                                },
-                            ]
-                            + gui_tracks
-                        ),
-                        disabled=not current_review,
-                    ),
-                ]
+    def create(self, product_name, instance_data, pre_create_data):
+        super().create(
+            product_name,
+            instance_data,
+            pre_create_data)
+
+        if len(self.selected) < 1:
+            return None
+
+        self.log.info(self.selected)
+        self.log.debug(f"Selected: {self.selected}")
+
+        instance_data.update(pre_create_data)
+        instance_data["task"] = None
+
+        for item in self.selected:
+            self.log.info(f"selected item: {item} is type {type(item)}")
+
+            # get clip item attributes from discreet segment object
+            clip_data = {
+                "PyClip": item,
+                "fps": float(str(item.frame_rate)[:-4])
+            }
+
+            attrs = [
+                "name", "width", "height",
+                "ratio", "sample_rate", "bit_depth"
+            ]
+
+            for attr in attrs:
+                val = getattr(item, attr)
+                clip_data[attr] = val
+
+            version = item.versions[-1]
+            track = version.tracks[-1]
+            for segment in track.segments:
+                segment_data = lib.get_segment_attributes(segment)
+                clip_data.update(segment_data)
+
+            # set instance related data
+            clip_index = str(uuid.uuid4())
+            clip_instance_data = deepcopy(instance_data)
+            clip_instance_data["clip_index"] = clip_index
+
+            instance = CreatedInstance(
+                self.product_type,
+                instance_data["productName"],
+                clip_instance_data,
+                self
+            )
+            self._add_instance_to_context(instance)
+            instance.transient_data["has_promised_context"] = True
+
+            instance.transient_data["clip_item"] = item
+            pipeline.imprint(
+                item,
+                data={
+                    _CONTENT_ID: clip_instance_data,
+                    "clip_data": clip_data,
+                }
             )
 
-        return instance_attributes
+    def collect_instances(self):
+        """Collect all created instances from current timeline."""
+        pass
 
+    def update_instances(self, update_list):
+        """Never called, update is handled via _FlameInstanceCreator."""
+        pass
 
-class EditorialPlateInstanceCreator(_FlameInstanceClipCreatorBase):
-    """Plate product type creator class"""
-    identifier = "io.ayon.creators.flame.plate"
-    product_type = "plate"
-    label = "Editorial Plate"
-
-    def create(self, instance_data, _):
-        """Return a new CreateInstance for new shot from Resolve.
-
-        Args:
-            instance_data (dict): global data from original instance
-
-        Return:
-            CreatedInstance: The created instance object for the new shot.
-        """
-        return super().create(instance_data, None)
+    def remove_instances(self, instances):
+        """Never called, update is handled via _FlameInstanceCreator."""
+        pass
