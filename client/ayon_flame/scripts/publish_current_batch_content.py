7f762579871a105a17c524b394534f2a9150956d
Robin DE LILLO <robindelillo@gmail.com>
2026-07-07 23:33:53 -0400

    Address feedback from PR.

--- client/ayon_flame/scripts/publish_current_batch_content.py
+++ client/ayon_flame/scripts/publish_current_batch_content.py
@@ -2,10 +2,8 @@
 """
 from qtpy import QtWidgets
 
-from ayon_core.tools.utils.host_tools import (
-    get_tool_by_name,
-    show_publisher,
-)
+from ayon_core.tools.publisher.control import PublisherController
+
 
 import ayon_flame.api as ayfapi
 
@@ -32,30 +30,6 @@ def publish_current_batch_content():
     """
     ayfapi.CTX.context = "FlameMenuBatch"
 
-    show_publisher(
-        tab="publish",
-        parent=_get_main_window()
-    )
-    window = get_tool_by_name("publisher")
-    window.set_current_tab("publish")
-    controller = window.controller
-
-    def on_ready_to_publish(_=None):
-        # Limit callback execution to this first call only.
-        if getattr(on_ready_to_publish, "done", False):
-            return  # already executed
-        on_ready_to_publish.done = True
-
-        controller.save_changes()
-        if not controller.publish_is_running():
-            controller.publish()
-
-    if window.isVisible() and not window._reset_on_show:
-        on_ready_to_publish()
-    else:
-        controller.register_event_callback(
-            "controller.reset.finished",
-            on_ready_to_publish
-        )
-
-    window.hide()
+    controller = PublisherController()
+    controller.reset()
+    controller.publish()
