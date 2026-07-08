55899c4feb7cd23dade6ae898e771258d6d99599
Robin De Lillo <robin@ynput.io>
2026-07-08 09:11:41 -0400

    Apply suggestions from code review
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_flame/scripts/publish_current_batch_content.py
+++ client/ayon_flame/scripts/publish_current_batch_content.py
@@ -1,28 +1,8 @@
 """ Publish current content without opening the UI.
 """
-from qtpy import QtWidgets
-
 from ayon_core.tools.publisher.control import PublisherController
 
-
 import ayon_flame.api as ayfapi
-
-
-_MAIN_WINDOW = None
-
-
-def _get_main_window():
-    global _MAIN_WINDOW
-    if _MAIN_WINDOW is None:
-        _MAIN_WINDOW = next(
-            (
-                obj
-                for obj in QtWidgets.QApplication.topLevelWidgets()
-                if isinstance(obj, QtWidgets.QMainWindow)
-            ),
-            None
-        )
-    return _MAIN_WINDOW
 
 
 def publish_current_batch_content():
